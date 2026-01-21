ae0601bb08fc8c19b3003728c94034ce77b91a29
Delta-Student27 <tanushripatil26@gmail.com>
2026-01-21 14:53:44 +0530

    Project Initiate

---
+++ src/main/java/com/example/demo/model/Role.java
@@ -0,0 +1,11 @@
+package com.example.demo.model;
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private RoleName name;
+}
